7bac18127a84aaf3763111c12a5aef377463039a
root <root@arjun-veettil.localdomain>
2022-08-18 15:18:12 +0200

    corrected the loop

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 cat /dev/null >  /root/csvserver/solution/inputFile
-
+INPUTFILE="/root/csvserver/solution/inputFile"
 if [ $# == 0 ]; then 
 	echo "no arguments"
+	echo "generating 10 entries!!!"
+	N=10
 else 
 	echo "$# number of arguments"
+	N=$1
 fi
 
-for i  in {0..$n}
-do
-    echo -e "$i, ${RANDOM}">> /root/csvserver/solution/inputFile
+for ((i = 0; i <= $N; i++)); do
+
+       	echo "$i, ${RANDOM}">> $INPUTFILE
 done
+
+
